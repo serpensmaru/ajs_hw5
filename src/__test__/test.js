5a858998e93b3b977f7542b6afddf186071c6bb0
Vladimir Kolosov <vovakolos99>
2023-03-14 21:16:02 +0500

    update

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,37 +1,33 @@
-import {getStatusHealth, sortedListHeroes} from "../myFunc";
+import { getStatusHealth, sortedListHeroes } from '../myFunc';
 
+test('get status health heroes', () => {
+  const params = { name: 'Маг', health: 90 };
+  const result = getStatusHealth(params);
+  expect(result).toBe('healthy');
+});
 
-test("get status health heroes", () => {
-    const params = {name: 'Маг', health: 90};
-    const result = getStatusHealth(params)
-    expect(result).toBe("healthy")
-})
+const listParams = [
+  [{ name: 'мечник', health: 10 }, 'critical'],
+  [{ name: 'маг', health: 45 }, 'wounded'],
+  [{ name: 'лучник', health: 80 }, 'healthy'],
+];
 
-const listParams  = [
-    [{name: 'мечник', health: 10}, "critical"],
-    [{name: 'маг', health: 45}, "wounded"],
-    [{name: 'лучник', health: 80}, "healthy"],
-  ]
+test.each(listParams)('test health= %o => %s ', (health, expected) => {
+  const result = getStatusHealth(health);
+  expect(result).toBe(expected);
+});
 
-test.each(listParams)
-("test health= %o => %s ", (health, expected) => {
-    const result = getStatusHealth(health)
-    expect(result).toBe(expected)
-})
- 
-
-test("sorted heroes on helth", () => {
-    const listParamsNoSort  = [
-        {name: 'мечник', health: 10},
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-      ]
-    const amountSorted  = [
-        { name: 'маг', health: 100 },
-        { name: 'лучник', health: 80 },
-        { name: 'мечник', health: 10 }
-      ]
-      const result = sortedListHeroes(listParamsNoSort)
-      expect(result).toEqual(amountSorted)
-    
-})
+test('sorted heroes on helth', () => {
+  const listParamsNoSort = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const amountSorted = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const result = sortedListHeroes(listParamsNoSort);
+  expect(result).toEqual(amountSorted);
+});
--- src/myFunc.js
+++ src/myFunc.js
@@ -1,14 +1,11 @@
 export function getStatusHealth(ojbStatus) {
-    const health = ojbStatus.health;
-    if (health > 50) {
-        return "healthy"
-    } else {
-        return health < 15 ? "critical": "wounded"
-     }
+  const { health } = ojbStatus;
+  if (health > 50) {
+    return 'healthy';
+  }
+  return health < 15 ? 'critical' : 'wounded';
 }
 
 export function sortedListHeroes(listParams) {
-    return listParams.sort(function(a, b) {
-        return parseFloat(b.health) - parseFloat(a.health);
-    });
+  return listParams.sort((a, b) => parseFloat(b.health) - parseFloat(a.health));
 }
